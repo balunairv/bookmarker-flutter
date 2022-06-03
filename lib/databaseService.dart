21b41a91862eaeab4ec8c9447547c4a6e8d29699
Balu Nair V <baluprasadam@gmail.com>
2022-06-03 20:41:14 +0530

    confirm

--- lib/databaseService.dart
+++ lib/databaseService.dart
@@ -2,14 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseServices {
   final String uid;
+
   DatabaseServices({required this.uid});
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('users');
-  Future addBookmark(String title, String url, String folder) async {
-    return await userCollection.doc(uid).set({
+  Future addBookmark({title, url, folder}) async {
+    final CollectionReference folderCollection = FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .collection(folder);
+    return await folderCollection.doc(title).set({
       'title': title,
       'url': url,
-      'folder': folder,
+      // 'folder': folder,
     });
   }
 }
